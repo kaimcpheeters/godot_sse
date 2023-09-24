ee7fe5bc2a4da7734c6bd328f62a3714d7dd32e6
Kai N.B. McPheeters <kaimcpheeters@gmail.com>
2023-09-24 19:10:59 -0400

    Fix localhost config setup

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,15 @@
 extends Node
 
 var config = {
- "url":"http://127.0.0.1:5000/" 
+ "host":"localhost",
+ "sub_url":"/",
+ "port":5000
 }
 
 func _ready():
-	var sub_url = ""
-	print('Connecting to:', config.url + sub_url)
+	print('Connecting to:', config)
 	$HTTPSSEClient.connect("connected", self, "on_connected")
-	$HTTPSSEClient.connect_to_host(config.url, sub_url, 80, false, false)
+	$HTTPSSEClient.connect_to_host(config.host, config.sub_url, config.port, false, false)
 
 func on_connected():
 	$HTTPSSEClient.connect("new_sse_event", self, "on_new_sse_event")
