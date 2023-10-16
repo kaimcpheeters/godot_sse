6d0abc69ea4e7c7338d17d507b5a5e367c75a20e
Kai N.B. McPheeters <kaimcpheeters@gmail.com>
2023-10-16 14:56:13 -0400

    Add stream-open event

--- sse_server.js
+++ sse_server.js
@@ -6,6 +6,9 @@ http.createServer((req, res) => {
   res.setHeader('Connection', 'keep-alive');
   res.flushHeaders();
 
+  // send a stream-open event immediately upon connection
+  res.write('event: stream-open\n\n');
+
   // send an event every second
   setInterval(() => {
     res.write(`event: example data: "${new Date().toISOString()}"\n\n`);
